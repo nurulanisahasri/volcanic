c5094078026f495d5cb193bb546cd360a847bb00
nurulanisahasri <85614688+nurulanisahasri@users.noreply.github.com>
2024-09-17 06:00:44 +0000

    Setting up landing page

---
+++ script.js
@@ -0,0 +1,14 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #f8f9fa;
+    color: #333;
+    font-family: Arial, sans-serif;
+}
+
+.container {
+    text-align: center;
+}
